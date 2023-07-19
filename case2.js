62289c495ac4ca8cf3cc6f690f9432789e515a0b
samettinho <smtyilmz@hotmail.com>
2023-07-19 11:01:11 +0300

    case1

---
+++ case2.js
@@ -0,0 +1,36 @@
+const fruit=["banana","banana","apple","fig","banana",
+	"cherry","apple","banana","orange","orange","Coconut",
+  "cherry","Avocado","banana","Avocado","Coconut","orange",
+	"Kiwi","Kiwi","Lemon","Lime"];
+	const array=[];
+	fruit.reduce((obj, fruitName)=>{
+		if (!array.some(f => Object.keys(f)[0] === fruitName)){
+			array.push({[fruitName]:{count:1}});
+		} else {
+			for (let j = 0; j < array.length; j++) {
+				const key = String(Object.keys(array[j]));
+				if(key===fruitName){
+					array[j][fruitName]['count']+=1;
+				}	
+			}
+		}
+	},{});
+	console.log(array);
+
+function sorting(params) {
+  switch (params) {
+      case 'asc':
+				console.log("--Küçükten büyüğe sıralama--");
+        array.sort((a,b) =>a[Object.keys(a)].count-b[Object.keys(b)].count);
+        console.log(array);
+	    	break;
+      case 'desc':
+				console.log("--Büyükten küçüğe sıralama--");
+        array.sort((a,b) =>b[Object.keys(b)].count-a[Object.keys(a)].count);
+        console.log(array);
+        break;
+			default:
+        break;
+  }
+}
+sorting('desc');
